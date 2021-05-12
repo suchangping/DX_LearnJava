9a5d810469d62bb7d0eb71fbdc8def896c5ca64b
10072002su_changping@cn.tre-inc.com <10072002su_changping@cn.tre-inc.com>
2021-05-12 13:18:58 +0900

    勉強

--- src/chapter02/Structure.java
+++ src/chapter02/Structure.java
@@ -1,4 +1,12 @@
 package chapter02;
 
 public class Structure {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        a = 20;
+        b = a + 5;
+        System.out.println(a);
+        System.out.println(b);
+    }
 }
