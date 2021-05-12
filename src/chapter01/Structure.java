b00ab2054912566ae260e2011b2d5e4269e5eb32
10072002su_changping@cn.tre-inc.com <10072002su_changping@cn.tre-inc.com>
2021-05-12 11:05:33 +0900

    第一章 識別子　しきべつし　identifier 标识符

---
+++ src/chapter01/Structure.java
@@ -0,0 +1,14 @@
+package chapter01;
+
+public class Structure {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        int pie = 5;
+        System.out.println("半径" + pie + "cmのパイの面積は、");
+        System.out.println(pie * pie * pi);
+
+        pi = 10;
+        System.out.println("半径" + pie + "cmのパイの面積は、");
+        System.out.println(pie * pie * pi);
+    }
+}
---
+++ src/chapter01/task1.java
@@ -0,0 +1,10 @@
+package chapter01;
+
+public class task1 {
+    public static  void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = a + b;
+        System.out.println("縦:" + a + "横:" + b + "長方形の面積は、" + c);
+    }
+}
